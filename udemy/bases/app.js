4d5e23fecedea560205f4a2ce1842d7e60158a3a
Fabián Mendoza <fmendoza.ospina7@outlook.com>
2023-12-26 09:36:38 -0500

    Knowledge Test

--- udemy/bases/app.js
+++ udemy/bases/app.js
@@ -1,5 +1,39 @@
 "use strict";
 (() => {
-    const msg = "Hola Mundo!";
-    console.log(msg);
+    // Tipos
+    const batman = 'Bruce';
+    const superman = 'Clark';
+    const existe = false;
+    // Tuplas
+    const parejaHeroes = [batman, superman];
+    const villano = ['Lex Lutor', 5, true];
+    // Arreglos
+    const aliados = ['Mujer Maravilla', 'Acuaman', 'San', 'Flash'];
+    //Enumeraciones
+    let Fuerza;
+    (function (Fuerza) {
+        Fuerza[Fuerza["acuaman"] = 0] = "acuaman";
+        Fuerza[Fuerza["batman"] = 1] = "batman";
+        Fuerza[Fuerza["flash"] = 5] = "flash";
+        Fuerza[Fuerza["superman"] = 100] = "superman";
+    })(Fuerza || (Fuerza = {}));
+    const fuerzaSuperman = Fuerza.superman;
+    const fuerzaFlash = Fuerza.flash;
+    const fuerzaBatman = Fuerza.batman;
+    const fuerzaAcuaman = Fuerza.acuaman;
+    // Retorno de funciones
+    function activar_batiseñal() {
+        return 'activada';
+    }
+    function pedir_ayuda() {
+        console.log('Auxilio!!!');
+    }
+    // Aserciones de Tipo
+    const poder = '100';
+    const largoDelPoder = poder.length;
+    console.log(largoDelPoder);
+    let string = `1.
+    2.
+    3.`;
+    console.log(string);
 })();
