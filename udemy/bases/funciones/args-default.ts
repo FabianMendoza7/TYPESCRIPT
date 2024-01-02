3c010e5662e5ecf88fc1c0e54c2c27ecb3086e57
Fabián Mendoza <fmendoza.ospina7@outlook.com>
2024-01-02 09:11:33 -0500

    By default params

---
+++ udemy/bases/funciones/args-default.ts
@@ -0,0 +1,14 @@
+(()=>{
+    const fullName = (firstName: string, lastName?: string, upper:boolean = false): string => {
+
+        if(upper){
+            return `${firstName} ${lastName || 'no-lastName'}`.toUpperCase();
+        } else {
+            return `${firstName} ${lastName || 'no-lastName'}`;
+        }
+    }
+
+    const name = fullName('Tony', 'Spark', true);
+
+    console.log({name});
+})()
